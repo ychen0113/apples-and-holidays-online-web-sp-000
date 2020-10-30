f8d6da058686240e0c284d2dcf41866efa95039d
Yiliang Chen <yiliangchen0113@gmail.com>
2020-10-30 18:13:32 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,9 +27,9 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
 
   holiday_hash[:winter].each do |holiday, supply|
-    stuff << supply
+    holiday.each do |x|
+      x << supply
   end
-  holiday_hash
 end
 
 
