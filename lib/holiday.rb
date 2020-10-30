91454233f7d7a8eb7751369de9418c4e8f262565
Yiliang Chen <yiliangchen0113@gmail.com>
2020-10-30 18:17:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -26,8 +26,8 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
 
-  holiday_hash[:winter].each do |holiday, data|
-    data << supply
+  holiday_hash[:winter].each do |key, value|
+    value << supply
   end
 end
 
@@ -35,6 +35,7 @@ end
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
+  
 
 end
 
