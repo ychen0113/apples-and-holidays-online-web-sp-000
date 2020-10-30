63ad21d5b20d637ead851f3e058a99db0dd4e13c
Yiliang Chen <yiliangchen0113@gmail.com>
2020-10-30 18:38:35 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -64,7 +64,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   
   holiday_hash.each do |season|
-    return season.to_s.capitalize 
+    puts season.to_s.
     season.each do |holiday, supply|
       return holiday.to_s.titlize supply.join.titlize
 end
