1c592f165900379114a1fd67206291c5bac2b909
Yiliang Chen <yiliangchen0113@gmail.com>
2020-10-30 18:41:57 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,9 +63,11 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   
-  holiday_hash.each do |season|
-    puts season.to_s.capitalize! + ":"
+  holiday_hash.each do |season, holiday|
+    puts season
 end
+
+
 end
 
 def all_holidays_with_bbq(holiday_hash)
